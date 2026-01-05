fa33315a978cf295418d5d4ca09e3a153f15aba0
yunsu-shin <yunsu102896@gmail.com>
2026-01-05 14:29:06 +0900

    chore: navbar href 변경

--- src/constants/navigation.constants.ts
+++ src/constants/navigation.constants.ts
@@ -6,9 +6,9 @@ export interface NavLink {
 export const NAV_LINKS: NavLink[] = [
   { href: '/', label: '홈' },
   { href: '/service', label: '서비스 소개' },
-  { href: '/guide', label: '이용안내' },
-  { href: '/mock-vote', label: '모의투표보기' },
-  { href: '/support', label: '고객센터' },
+  { href: '/userguide', label: '이용안내' },
+  { href: '/requestquote', label: '모의견적보기' },
+  { href: '/customercenter', label: '고객센터' },
 ];
 
 export const LANGUAGES = ['KO', 'EN'] as const;
